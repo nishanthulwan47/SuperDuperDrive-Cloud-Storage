d88bf75c1b33be45363fe17bcf187217119298b9
Nishant Hulwan <nishanthulwan47@gmail.com>
2020-11-15 19:32:02 -0500

    first commit

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/User.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/User.java
@@ -1,4 +1,57 @@
 package com.udacity.jwdnd.course1.cloudstorage.model;
 
 public class User {
+    private String UserId;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+
+    public User(String userId, String firstName, String lastName, String username, String password) {
+        UserId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(String userId) {
+        UserId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
