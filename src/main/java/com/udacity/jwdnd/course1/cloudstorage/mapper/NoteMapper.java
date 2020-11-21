a0777bc1af94d73bdff2cd892c590dddf3ca2aab
Nishant Hulwan <nishanthulwan47@gmail.com>
2020-11-20 22:21:38 -0500

    database not working

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
@@ -1,9 +1,21 @@
 package com.udacity.jwdnd.course1.cloudstorage.mapper;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface NoteMapper {
 
+    @Select("SELECT * FROM NOTES")
+    List<Note> getAllNotes();
+
+    @Select("SELECT * FROM NOTES WHERE noteid = #{noteid}")
+    List<Note> getAllNoteById();
+
+    @Select("SELECT * FROM NOTES WHERE userid = #{userid}")
+    List<Note> getAllNoteByUserId();
 
 }
