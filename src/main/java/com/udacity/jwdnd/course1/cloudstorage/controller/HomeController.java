3e3580f9392cbdd7dc6e21a3c57937bbeeb09471
Nishant Hulwan <nishanthulwan47@gmail.com>
2020-11-21 23:17:14 -0500

    implementing file upload

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -2,9 +2,7 @@ package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
 import com.udacity.jwdnd.course1.cloudstorage.model.AppUser;
-import com.udacity.jwdnd.course1.cloudstorage.services.AppUserService;
-import com.udacity.jwdnd.course1.cloudstorage.services.AuthenticationService;
-import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
+import com.udacity.jwdnd.course1.cloudstorage.services.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,15 +18,18 @@ public class HomeController {
 
 
     private final AppUserService appUserService;
-
-
+    private final CredentialService credentialService;
+    private final NoteService noteService;
     private final FileService fileService;
 
 
-    public HomeController(AuthenticationService authenticationService, AppUserService appUserService, UserMapper userMapper, FileService fileService) {
+    public HomeController(AuthenticationService authenticationService, AppUserService appUserService,
+                          UserMapper userMapper, CredentialService credentialService, NoteService noteService,
+                          FileService fileService) {
 
         this.appUserService = appUserService;
-
+        this.credentialService = credentialService;
+        this.noteService = noteService;
         this.fileService = fileService;
     }
 
@@ -38,7 +39,7 @@ public class HomeController {
     }
 
     @PostMapping
-    public String getHomePage(Authentication authentication, Model model) throws IOException {
+    public String UploadFiles(Authentication authentication, Model model) throws IOException {
 
         AppUser appUser = appUserService.getUser(authentication.getPrincipal().toString());
         Integer userId = appUser.getUserid();
--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialService.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialService.java
@@ -1,6 +1,63 @@
 package com.udacity.jwdnd.course1.cloudstorage.services;
 
+import com.udacity.jwdnd.course1.cloudstorage.mapper.CredentialMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Service
 public class CredentialService {
 
+    private CredentialMapper credentialMapper;
 
+    private EncryptionService encryptionService;
+
+    public CredentialService(EncryptionService encryptionService) {
+        this.encryptionService = encryptionService;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        System.out.println("CredentialService Bean");
+    }
+
+    public void addCredential(Credential credential) {
+        String key = this.encryptionService.generateKey();
+        credential.setKey(key);
+        credential.setPassword(this.encryptPassword(credential));
+        this.credentialMapper.insert(credential);
+    }
+
+    public List<Credential> getAllCredentials() {
+        return this.credentialMapper.getAllCredentials();
+    }
+
+    public List<Credential> getCredentialByUserId(Integer id) {
+        return this.credentialMapper.getAllCredentialByUserId(id);
+    }
+
+    public Credential getCredentialById(Integer id) {
+        return this.credentialMapper.getCredentialId(id);
+    }
+
+    public void deleteCredential(Integer id) {
+        this.credentialMapper.delete(id);
+    }
+
+    public String retrieveKeyByCredentialId(Integer id) {
+        return this.credentialMapper.retrieveKeyByCredentialId(id);
+    }
+
+    public void editCredential(Credential credential) {
+        String key = this.credentialMapper.retrieveKeyByCredentialId(credential.getCredentialid());
+        String encodedPassword = this.encryptionService.encryptValue(credential.getPassword(), key);
+        credential.setPassword(encodedPassword);
+        this.credentialMapper.update(credential);
+    }
+
+    public String encryptPassword(Credential credential) {
+        return this.encryptionService.encryptValue(credential.getPassword(), credential.getKey());
+    }
 }
