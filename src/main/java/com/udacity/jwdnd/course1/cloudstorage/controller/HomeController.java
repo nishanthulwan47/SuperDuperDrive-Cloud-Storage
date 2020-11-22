62a891a8e06ce9337157f81d408675fdb6f7c8d6
Nishant Hulwan <nishanthulwan47@gmail.com>
2020-11-22 00:07:28 -0500

    implementing file upload

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -2,6 +2,9 @@ package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
 import com.udacity.jwdnd.course1.cloudstorage.model.AppUser;
+import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
+import com.udacity.jwdnd.course1.cloudstorage.model.File;
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
 import com.udacity.jwdnd.course1.cloudstorage.services.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -9,6 +12,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 
@@ -38,13 +44,59 @@ public class HomeController {
         return "home";
     }
 
-    @PostMapping
-    public String UploadFiles(Authentication authentication, Model model) throws IOException {
+    @GetMapping("/home")
+    public String ListUploadFiles(Authentication authentication, Model model) throws IOException {
 
         AppUser appUser = appUserService.getUser(authentication.getPrincipal().toString());
         Integer userId = appUser.getUserid();
         model.addAttribute("files", this.fileService.getAllFilesByUserId(userId));
-        return "";
+        model.addAttribute("credentials", this.credentialService.getCredentialByUserId(userId));
+        model.addAttribute("notes", this.noteService.getAllNotesByUserId(userId));
+        model.addAttribute("newNote", new Note());
+        //model.addAttribute("newCredential", new Credential());
+
+        model.addAttribute("credentialService", credentialService);
+        return "home";
     }
+
+    @PostMapping("/logout")
+    public String logout() {
+        return "login?logout";
+    }
+    @GetMapping("/logout")
+    public String logoutView() {
+        return "redirect:/login?logout";
+    }
+
+    public String uploadFile(@RequestParam("fileUpload") Authentication auth, Model model,
+                             RedirectAttributes redirectAttributes,
+                             MultipartFile fileUpload) {
+
+        AppUser appUser = appUserService.getUser(auth.getPrincipal().toString());
+        model.addAttribute("activeTab", "files");
+        model.addAttribute("errorMessage", false);
+        model.addAttribute("successMessage", false);
+
+        if (fileUpload.getOriginalFilename().isEmpty()) {
+            model.addAttribute("errorMessage", "Please select file to upload");
+            return "result";
+        }
+        if (!fileService.isFilenameAvailable(fileUpload.getOriginalFilename(), appUser.getUserid())) {
+            model.addAttribute("errorMessage", "File with same filename already exists");
+            return "result";
+        }
+
+        try {
+            fileService.uploadFile(new File(fileUpload.getOriginalFilename(), fileUpload.getContentType(),
+                    fileUpload.getSize() + "", appUser.getUserid(), fileUpload.getBytes()));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            model.addAttribute("errorMessage", e.getMessage());
+        }
+
+    }
+
+
 
 }
